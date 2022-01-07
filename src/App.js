8569c60b394dbd026dcb7f580574285f7650a36d
alexjun12 <alexjun12@naver.com>
2022-01-07 17:13:15 +0900

    2022_01_07commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+}from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function App() {
-  return (
-    <div>
-    </div>
-  );
+  return <Router>
+    <Routes>
+      <Route path = "/movie/:id" element = {<Detail />}></Route>
+      <Route path = "/" element = {<Home />}></Route>
+    </Routes>
+  </Router>;
 }
+
 
 export default App;
